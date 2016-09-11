069b2b4c9461ffaea3abeb1fbda8c9caf61600e2
TINAYAO\juvia <tinayao333@gmail.com>
2016-09-11 22:49:52 +1000

    A number of changes: - Added a function to retrive article from Trove - Included and fixed Phuong's textbox change - Fixed the blue border from the description tab - etc.

--- Australian Wildlife/javascript/script.js
+++ Australian Wildlife/javascript/script.js
@@ -1,34 +1,66 @@
-$(document).ready(function(){
+var apiKey = "laktuo6dkdd22o5m";
+var animalname = "";
 
+$(document).ready(function(){
 // Get the modal
 var modal = document.getElementById('myModal');
-
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
-
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal
-btn.onclick = function() {
+$("img").click(function(){
+	animalname = $(this).attr('id');
     modal.style.display = "block";
-	document.getElementById('focusmeplease').focus();
 	openCity(event, 'Description');
-}
+	getArticle();
+});
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
+	$("#Article").html("<h3>" + "Article" + "</h3>" + "<p>" + "loading..." +"</p>");
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
+		$("#Article").html("<h3>" + "Article" + animalname + "</h3>" + "<p>" + "loading..." +"</p>");
     }
 }
 
 });
+
+function getArticle(){
+// Get the values from our search form
+	var searchTerm = animalname;			  
+	var searchZone = "newspaper";
+	var sortBy = "relevance";
+	var url = "http://api.trove.nla.gov.au/result?key=" + apiKey + "&encoding=json&zone=" + searchZone + "&sortby=" + sortBy + "&q=" + searchTerm + "&s=0&n=5&include=articletext,pdf&encoding=json&callback=?";
+	var text=null;
+	var count=0;
+	var index = 0;
+				    
+	console.log(url);
+			    	
+	$.getJSON(url, function(data) {
+		// clear the HTML div that will display the results
+		$.each(data.response.zone[0].records.article, function(index, value) {
+			count++
+		});
+		var number = Math.floor(Math.random() * count) + 1;
+		console.log(number);
+		$.each(data.response.zone[0].records.article, function(index, value) {
+			if (index==number){
+				$("#Article").html("<h3>" + "Article from Trove about " + animalname + "</h3>" + "<p>" + value.articleText +"</p>");
+			}
+		});
+	});
+					
+	
+}
 
 function openCity(evt, cityName) {
 	event.preventDefault();
@@ -39,9 +71,14 @@ function openCity(evt, cityName) {
     }
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
+		$(tablinks[i]).parent().css("background-color", "#ECECEC");
+    }  
+	if (evt.currentTarget.id==animalname){
+		$(tablinks[0]).parent().css("background-color", "#ccc");
+	}
+	$(evt.currentTarget).parent().css("background-color", "#ccc");
     document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
-	
 }
+
+
+
