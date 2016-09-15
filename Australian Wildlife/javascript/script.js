4bae3ed7d0e374929c4cb438f7ae2d41aa86c3a0
TINAYAO\juvia <tinayao333@gmail.com>
2016-09-15 23:07:54 +1000

    The textbox will move between each sections. Fixed some minor functions and graphics.

--- Australian Wildlife/javascript/script.js
+++ Australian Wildlife/javascript/script.js
@@ -5,48 +5,56 @@ var loadedImages = [];
 var urlPatterns = ["flickr.com", "nla.gov.au", "artsearch.nga.gov.au", "recordsearch.naa.gov.au", "images.slsa.sa.gov.au"];
 var found = 0;
 
-$(document).ready(function(){
 
-
+$(document).ready(function(){	
+	var hidden_index =  $("#koala").css("z-index")
+	if (hidden_index == 0){
+		$( "#hidden" ).hide();
+	}
 	$("#koala").click(function(){
-	  $("#koala").css({"z-index": 2});
+		$("#koala").css({"z-index": 2});
+		$( "#hidden" ).show();
 	});
 
 
-// Get the modal
-var modal = document.getElementById('myModal');
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+	// Get the modal
+	var modal = document.getElementById('myModal');
+	// Get the button that opens the modal
+	var btn = document.getElementById("myBtn");
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal
-$(".more_info").click(function(){
-	animalname = $(this).attr('id');
-    modal.style.display = "block";
-	openCity(event, 'Description');
-	getArticle();
-	getImage();
-
-});
+	$(".more_info").click(function(){
+		var habitat = $(this).parent();
+		var habitat_id = $(this).parent().attr('id');
+		habitat.append(modal);
+		animalname = $(this).attr('id');
+		if (habitat_id=="grassland"){
+			$('.modal-content').css({"margin-left":"50%"});
+			$('.tab').css({"margin-left":"50%"});
+		}
+		modal.style.display = "block";
+		openCity(event, 'Description');
+		getArticle();
+		getImage();
+	});
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-	$("#Article").html("<h3>" + "Article" + "</h3>" + "<p>" + "loading..." +"</p>");
-	$("#Images").html("<h3>" + "Images" + "</h3>" + "<p>" + "loading..." +"</p>");
-}
+	span.onclick = function() {
+		modal.style.display = "none";
+		$("#Article").html("<h3>" + "Article" + "</h3>" + "<p>" + "loading..." +"</p>");
+		$("#Images").html("<h3>" + "Images" + "</h3>" + "<p>" + "loading..." +"</p>");
+	}
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-		$("#Article").html("<h3>" + "Article" + "</h3>" + "<p>" + "loading..." +"</p>");	
-		$("#Images").html("<h3>" + "Images" + "</h3>" + "<p>" + "loading..." +"</p>");
-		
-    }
-}
-
+	window.onclick = function(event) {
+		if (event.target == modal) {
+			modal.style.display = "none";
+			$("#Article").html("<h3>" + "Article" + "</h3>" + "<p>" + "loading..." +"</p>");	
+			$("#Images").html("<h3>" + "Images" + "</h3>" + "<p>" + "loading..." +"</p>");	
+		}
+	}
 });
 
 function getArticle(){
@@ -64,16 +72,16 @@ function getArticle(){
 	$.getJSON(url, function(data) {
 		// clear the HTML div that will display the results
 		$.each(data.response.zone[0].records.article, function(index, value) {
-			count++
-		});
-		var number = Math.floor(Math.random() * count) + 1;
-		console.log(number);
-		$.each(data.response.zone[0].records.article, function(index, value) {
-			if (index==number){
-				$("#Article").html("<h3>" + "Article from Trove about " + animalname + "</h3>" + "<p>" + value.articleText +"</p>");
-			}
-		});
+		count++
 	});
+	var number = Math.floor(Math.random() * count) + 1;
+	console.log(number);
+	$.each(data.response.zone[0].records.article, function(index, value) {
+		if (index==number){
+			$("#Article").html("<h3>" + "Article from Trove about " + animalname + "</h3>" + "<p>" + value.articleText +"</p>");
+		}
+	});
+});
 					
 	
 }
@@ -97,7 +105,6 @@ function openCity(evt, cityName) {
 }
 
 function waitForFlickr() {
-	
 	if(found == loadedImages.length) {
 		printImages();
 	}else if(found+1 == loadedImages.length||found-1 == loadedImages.length){
@@ -115,18 +122,15 @@ function getImage(){
     var searchTerm = animalname;
     searchTerm = searchTerm.replace(/ /g, "%20");
     var sortBy = "relevance";
-
     //create searh query
     var url = "http://api.trove.nla.gov.au/result?key=" + apiKey + "&l-availability=y%2Ff&encoding=json&zone=picture" + "&sortby=relevance&n=100&q=" + searchTerm + "&callback=?";
     //get the JSON information we need to display the images
     $.getJSON(url, function(data) {
         console.log(data);
         $.each(data.response.zone[0].records.work, processImages);
-
-	waitForFlickr(); // Waits for the flickr images to load
+		waitForFlickr(); // Waits for the flickr images to load
     });
 }
-
 
 /*
 *   Depending where the image comes from, there is a special way to get that image from the website.
@@ -134,40 +138,33 @@ function getImage(){
 */
 function processImages(index, troveItem) {
     var imgUrl = troveItem.identifier[0].value;
-        if (imgUrl.indexOf(urlPatterns[0]) >= 0) { // flickr
+    if (imgUrl.indexOf(urlPatterns[0]) >= 0) { // flickr
 		found++;
-            addFlickrItem(imgUrl, troveItem);
-
-        } else if (imgUrl.indexOf(urlPatterns[1]) >= 0) { // nla.gov
+        addFlickrItem(imgUrl, troveItem);
+    } else if (imgUrl.indexOf(urlPatterns[1]) >= 0) { // nla.gov
 		found++;
-            loadedImages.push(
-                imgUrl + "/representativeImage?wid=900" // change ?wid=900 to scale the image
-            );
-
-        } else if (imgUrl.indexOf(urlPatterns[2]) >= 0) { //artsearch
+        loadedImages.push(
+            imgUrl + "/representativeImage?wid=900" // change ?wid=900 to scale the image
+        );
+    } else if (imgUrl.indexOf(urlPatterns[2]) >= 0) { //artsearch
 		found++;
-            loadedImages.push(
-                "http://artsearch.nga.gov.au/IMAGES/LRG/" + getQueryVariable("IRN", imgUrl) + ".jpg"
-            );
-
-        } else if (imgUrl.indexOf(urlPatterns[3]) >= 0) { //recordsearch
+        loadedImages.push(
+        "http://artsearch.nga.gov.au/IMAGES/LRG/" + getQueryVariable("IRN", imgUrl) + ".jpg"
+    );
+    } else if (imgUrl.indexOf(urlPatterns[3]) >= 0) { //recordsearch
 		found++;
-            loadedImages.push(
-                "http://recordsearch.naa.gov.au/NAAMedia/ShowImage.asp?T=P&S=1&B=" + getQueryVariable("Number", imgUrl)
-            );
-
-        } else if (imgUrl.indexOf(urlPatterns[4]) >= 0) { //slsa 
-            found++;
-            loadedImages.push(
-                imgUrl.slice(0, imgUrl.length - 3) + "jpg"
-		);
-            
-        } else { // Could not reliably load image for item
-            // UNCOMMENT FOR DEBUG: 
-	    console.log("Not available: " + imgUrl);
-        }
-
+        loadedImages.push(
+        "http://recordsearch.naa.gov.au/NAAMedia/ShowImage.asp?T=P&S=1&B=" + getQueryVariable("Number", imgUrl)
+        );
+    } else if (imgUrl.indexOf(urlPatterns[4]) >= 0) { //slsa 
+        found++;
+        loadedImages.push(
+        imgUrl.slice(0, imgUrl.length - 3) + "jpg"
+	);     
+    } else { // Could not reliably load image for item
+		console.log("Not available: " + imgUrl);
     }
+}
 
 function addFlickrItem(imgUrl, troveItem) {
     var flickr_key = "a4d0bf2f4bde0595521b7bd8317ec428";
@@ -199,14 +196,12 @@ function printImages() {
 			image.style.display="block";
 			image.style.margin = "auto";
 			image.style.verticalAlign = "top";
-
 				
 			$("#Images").html("<h3>" + "Images" + "</h3>" + "<p>" +"</p>");
 			$("#Images").append(image);
 		}
     }
-
-    }
+}
 
 // from http://css-tricks.com/snippets/javascript/get-url-variables/
 function getQueryVariable(variable, url) {
